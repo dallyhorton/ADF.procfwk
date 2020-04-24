2e6ce8d344c4c41cb49f33cb0d72efd675560f5f
Troy Holland <Troy.Holland@artc.com.au>
2020-04-24 16:47:29 +0930

    Fix ExecutionLogBackup Issue
    - When publishing to an empty database, we need to check for the existence of tables

--- MetadataDB/Scripts/LogData/ExecutionLogBackup.sql
+++ MetadataDB/Scripts/LogData/ExecutionLogBackup.sql
@@ -1,8 +1,9 @@
 ﻿IF OBJECT_ID(N'[dbo].[ExecutionLogBackup]') IS NOT NULL DROP TABLE [dbo].[ExecutionLogBackup];
 
-SELECT 
-	*
-INTO
-	[dbo].[ExecutionLogBackup]
-FROM
-	[procfwk].[ExecutionLog];
+IF OBJECT_ID(N'[procfwk].[ExecutionLog]') IS NOT NULL
+	SELECT 
+		*
+	INTO
+		[dbo].[ExecutionLogBackup]
+	FROM
+		[procfwk].[ExecutionLog];
